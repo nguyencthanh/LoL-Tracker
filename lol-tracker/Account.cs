215be24893f367b0fc23e4dec4b88e45584236c7
Thanh Nguyen <thanhnhotn2001@gmail.com>
2025-02-24 10:27:09 -0500

    Account Infromation Added

---
+++ lol-tracker/Account.cs
@@ -0,0 +1,9 @@
+namespace LoLTracker
+{
+    public class Account
+    {
+        public string puuid { get; set; }
+        public string gameName { get; set; }
+        public string tagLine { get; set; }
+    }
+}
---
+++ lol-tracker/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+
+namespace LoLTracker
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Getting API Key
+            string apiKey = ApiKey.GetApiKey();
+
+            HttpClient client = new HttpClient();
+
+            // Obtain User Information 
+            Console.WriteLine("What is your game name: ");
+            string gameName = Console.ReadLine();
+            Console.WriteLine("What is your tag name: ");
+            string tagLine = Console.ReadLine();
+            string url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + gameName + "/" + tagLine + "?api_key=" + apiKey;
+
+            try
+            {
+                // Performing the GET request
+                HttpResponseMessage respone = await client.GetAsync(url);
+
+                // If the request is successful
+                if (respone.IsSuccessStatusCode)
+                {
+                    string content = await respone.Content.ReadAsStringAsync();
+
+                    Account account = JsonSerializer.Deserialize<Account>(content);
+
+                    Console.WriteLine($"Puuid: {account.puuid}");
+                    Console.WriteLine($"Puuid: {account.gameName}");
+                    Console.WriteLine($"Puuid: {account.tagLine}");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
